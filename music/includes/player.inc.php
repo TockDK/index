5c897283fd83135a42bb7f87933ac10f3b3d6f00
TockDK <tobias2402@hotmail.dk>
2017-01-29 22:00:37 +0100

    Update player.inc.php

--- music/includes/player.inc.php
+++ music/includes/player.inc.php
@@ -1,12 +1,12 @@
+<link rel="stylesheet" href="style/style.css">
 <?php
 function getsongs($conn){
 	$sql = "SELECT * FROM music";
 	$result = $conn->query($sql);
-	while ($row = $result->fetch_assoc()){
-	echo "<div class='song-list'>";
-		echo $row['sname']." ";
-		echo $row['salbum']." ";
-		echo $row['sartist']."<br>";
-	echo "</div>";
-	}
+    echo "<table border='1'><tr><th>Song</th><th>album</th><th>Artist</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["sname"]."</td><td>".$row["salbum"]."</td><td>".$row["sartist"]."</td></tr>";
+    }
+    echo "</table>";
 }
